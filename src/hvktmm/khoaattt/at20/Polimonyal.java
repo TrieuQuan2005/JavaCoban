da8779e1dc1115508c70427c1cdbc21459e1a0e9
TrieuQuan2005 <Trieuquan.2206@gmail.com>
2025-11-11 01:37:58 +0700

    Loops Test Upload

---
+++ src/hvktmm/khoaattt/at20/Polimonyal.java
@@ -0,0 +1,75 @@
+package hvktmm.khoaattt.at20;
+
+import java.util.Scanner;
+
+public class Polimonyal {
+
+    static boolean CheckInteger(double x) {
+        return x == (int) x;
+    }
+
+    static double[] nhapHeSo(int bac) {
+        double[] heSo = new double[bac + 1];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = bac; i >= 0; i--) {
+            System.out.print("Nhập hệ số bậc " + i + ": ");
+            heSo[i] = sc.nextDouble();
+        }
+        return heSo;
+    }
+
+    static double tinhGiaTri(double[] heSo, double x) {
+        double ketQua = 0;
+        for (int i = heSo.length - 1; i >= 0; i--) {
+            ketQua = ketQua * x + heSo[i];
+        }
+        return ketQua;
+    }
+
+    static void inDaThuc(double[] heSo) {
+        int bac = heSo.length - 1;
+        StringBuilder daThuc = new StringBuilder();
+
+        for (int i = bac; i >= 0; i--) {
+            double hs = heSo[i];
+            if (hs == 0) continue;
+
+            // Dấu
+            if (!daThuc.isEmpty()) daThuc.append(hs > 0 ? " + " : " - ");
+            else if (hs < 0) daThuc.append("-");
+
+            double absHs = Math.abs(hs);
+
+            // In hệ số (bỏ nếu =1 và không phải bậc 0)
+            if (absHs != 1 || i == 0) {
+                if (CheckInteger(absHs)) daThuc.append((int) absHs);
+                else daThuc.append(absHs);
+            }
+
+            // In biến và mũ
+            if (i >= 1) {
+                daThuc.append("x");
+                if (i > 1) daThuc.append("^").append(i);
+            }
+        }
+
+        System.out.println("Đa thức: " + daThuc);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập bậc của đa thức: ");
+        int bac = sc.nextInt();
+
+        double[] heSo = nhapHeSo(bac);
+        inDaThuc(heSo);
+
+        System.out.print("Nhập giá trị x để tính đa thức: ");
+        double x = sc.nextDouble();
+
+        double ketQua = tinhGiaTri(heSo, x);
+        System.out.println("Giá trị của đa thức tại x = " + x + " là: " + ketQua);
+    }
+}
---
+++ src/hvktmm/khoaattt/at20/XepLoaiHocLuc1.java
@@ -0,0 +1,27 @@
+package hvktmm.khoaattt.at20;
+import java.util.Scanner;
+
+public class XepLoaiHocLuc1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập điểm (0 - 10): ");
+        int diem = sc.nextInt();
+        switch (diem) {
+            case 9:
+            case 10:
+                System.out.println("Xếp loại: Giỏi");
+                break;
+            case 7:
+            case 8:
+                System.out.println("Xếp loại: Khá");
+                break;
+            case 5:
+            case 6:
+                System.out.println("Xếp loại: Trung bình");
+                break;
+            default:
+                System.out.println("Xếp loại: Yếu");
+        }
+    }
+}
